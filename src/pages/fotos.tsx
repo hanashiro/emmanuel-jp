6bf9843cdeee9be4a0553ba3f1f8936ed88c28c9
hanashiro <felipeaah@gmail.com>
2021-05-09 01:23:28 -0300

    cache das fotos

--- src/pages/fotos.tsx
+++ src/pages/fotos.tsx
@@ -2,11 +2,32 @@ import Head from 'next/head';
 import { Dialog } from '@material-ui/core';
 import ContentBox from 'ui/components/data-display/ContentBox';
 import { picturesList } from 'data/database/pictures-data';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { PictureGrid } from '@styles/pages/fotos.styled';
 
 export default function Fotos() {
-    const [selectedPicture, setPicture] = useState('');
+    const [selectedPicture, setPicture] = useState(''),
+        picturesListElements = useMemo(
+            () =>
+                picturesList.reverse().map((pictureCollection) => (
+                    <ContentBox
+                        key={pictureCollection.name}
+                        title={pictureCollection.name.replace('/', '')}
+                    >
+                        <PictureGrid>
+                            {pictureCollection.files.map((picture) => (
+                                <img
+                                    key={picture}
+                                    src={`/img/fotos-thumb${picture}`}
+                                    onClick={() => setPicture(picture)}
+                                    loading="lazy"
+                                />
+                            ))}
+                        </PictureGrid>
+                    </ContentBox>
+                )),
+            [picturesList]
+        );
     return (
         <>
             <Head>
@@ -25,23 +46,7 @@ export default function Fotos() {
                 )}
             </Dialog>
 
-            {picturesList.reverse().map((pictureCollection) => (
-                <ContentBox
-                    key={pictureCollection.name}
-                    title={pictureCollection.name.replace('/', '')}
-                >
-                    <PictureGrid>
-                        {pictureCollection.files.map((picture) => (
-                            <img
-                                key={picture}
-                                src={`/img/fotos-thumb${picture}`}
-                                onClick={() => setPicture(picture)}
-                                loading="lazy"
-                            />
-                        ))}
-                    </PictureGrid>
-                </ContentBox>
-            ))}
+            {picturesListElements}
         </>
     );
 }
